ac4e1be1d7add756b737e05df4c53ba44fb07109
sutharimanikanta <111943981+sutharimanikanta@users.noreply.github.com>
2025-03-11 17:11:10 +0530

    Update config.py

--- intern/app/config.py
+++ intern/app/config.py
@@ -4,6 +4,6 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = (
-        os.environ.get("DATABASE_URL") or "sqlite:///resource_tracking.db"
+        os.environ.get("DATABASE_URL") or "connect this to ur server"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
